8e1edc04d9d84acf4379b1e389f9ad74ba03f3b4
Ipin22 <ucihamala573@gmail.com>
2026-05-22 11:21:12 +0000

    drivers: add qihoo drivers In CPH2083_A67, these drivers are highly required.
    Signed-off-by: Ipin22 <ucihamala573@gmail.com>

--- lib/bug.c
+++ lib/bug.c
@@ -47,6 +47,11 @@
 #include <linux/sched.h>
 
 extern const struct bug_entry __start___bug_table[], __stop___bug_table[];
+#ifdef VENDOR_EDIT
+#ifdef CONFIG_QIHOO
+extern const struct bug_entry *qihoo_patch_find_bug(unsigned long bugaddr);
+#endif
+#endif /* VENDOR_EDIT */
 
 static inline unsigned long bug_addr(const struct bug_entry *bug)
 {
@@ -134,6 +139,13 @@ const struct bug_entry *find_bug(unsigned long bugaddr)
 	for (bug = __start___bug_table; bug < __stop___bug_table; ++bug)
 		if (bugaddr == bug_addr(bug))
 			return bug;
+#ifdef VENDOR_EDIT
+#ifdef CONFIG_QIHOO
+	bug = qihoo_patch_find_bug(bugaddr);
+	if (bug)
+		return bug;
+#endif
+#endif /* VENDOR_EDIT */
 
 	return module_find_bug(bugaddr);
 }
